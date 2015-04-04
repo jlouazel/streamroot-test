0da29d5ec66c40718d2decd3ac171b7479155ffe
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2015-04-04 00:13:48 -0700

    Add Facebook app informations in application.

--- server/auth/facebook/index.js
+++ server/auth/facebook/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 router
   .get('/', passport.authenticate('facebook', {
-    scope: ['email', 'user_about_me'],
+    scope: ['email', 'public_profile', 'user_friends'],
     failureRedirect: '/signup',
     session: false
   }))

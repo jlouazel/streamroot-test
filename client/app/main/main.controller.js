c1249076a0ed7ae922a38ff40cc5391877dbe80a
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2015-04-11 21:34:30 -0700

    Basic communication successful with at the help of the server (signaling). Need to clear things.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,117 +1,212 @@
 'use strict';
 
 angular.module('streamrootTestApp')
-.controller('MainCtrl', function ($scope, Conversation) {
+.controller('MainCtrl', function ($scope, socket) {
+  socket.syncUpdates('chat', $scope.awesomeThings);
+
   $scope.message = '';
   $scope.messageQueue = [];
 
-  var servers = null,
-  sendChannel,
-  receiveChannel;
+  var isInitiator = false;
 
-  var localPeerConnection = window.localPeerConnection =
-  new webkitRTCPeerConnection(servers, {
-    optional: [{
-      RtpDataChannels: true
-    }]
+  var configuration = {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+
+  $scope.$on('$destroy', function () {
+    socket.unsyncUpdates('chat');
   });
 
-  console.log('Created local peer connection object localPeerConnection');
+  var room = 'plop';
 
-  try {
-    // Reliable Data Channels not yet supported in Chrome
-    sendChannel = localPeerConnection.createDataChannel('sendDataChannel', {
-      reliable: false
-    });
-    console.log('Created send data channel');
-  } catch (e) {
-    // alert('Failed to create data channel. ' +
-    // 'You need Chrome M25 or later with RtpDataChannel enabled');
-    console.log('createDataChannel() failed with exception: ' + e.message);
+  $scope.send = function() {
+    console.log('Client sending message: ', $scope.message);
+    // socket.syncUpdates('message', $scope.message);
+    $scope.message = '';
+  };
+
+
+  socket.socket.on('ipaddr', function (ipaddr) {
+    console.log('Server IP address is: ' + ipaddr);
+    // updateRoomURL(ipaddr);
+  });
+
+  socket.socket.on('created', function (room, clientId) {
+    console.log('Created room', room, '- my client ID is', clientId);
+    isInitiator = true;
+  });
+
+  socket.socket.on('joined', function (room, clientId) {
+    console.log('This peer has joined room', room, 'with client ID', clientId);
+    isInitiator = false;
+  });
+
+  socket.socket.on('ready', function () {
+    createPeerConnection(isInitiator, configuration);
+  })
+
+  socket.socket.on('log', function (array) {
+    console.log.apply(console, array);
+  });
+
+  socket.socket.on('message', function (message){
+    $scope.messageQueue.push({text: message});
+    console.log('Client received message:', message);
+    signalingMessageCallback(message);
+  });
+
+  // Join a room
+  socket.socket.emit('create or join', room);
+
+  if (location.hostname.match(/localhost|127\.0\.0/)) {
+    socket.socket.emit('ipaddr');
   }
-  localPeerConnection.onicecandidate = gotLocalCandidate;
-  sendChannel.onopen = handleSendChannelStateChange;
-  sendChannel.onclose = handleSendChannelStateChange;
 
-  remotePeerConnection = window.remotePeerConnection =
-  new webkitRTCPeerConnection(
-    servers, {
-      optional: [{
-        RtpDataChannels: true
-      }]
-    }
-  );
-  console.log('Created remote peer connection object remotePeerConnection');
-
-  remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
-  remotePeerConnection.ondatachannel = gotReceiveChannel;
-
-  localPeerConnection.createOffer(gotLocalDescription);
+  /**
+  * Send message to signaling server
+  */
+  $scope.sendMessage = function(){
+    console.log('Client sending message: ', $scope.message);
+    socket.socket.emit('message', $scope.message);
+    $scope.message = '';
+  }
 
 
-  function gotLocalCandidate(event) {
-    console.log('local ice callback');
-    if (event.candidate) {
-      remotePeerConnection.addIceCandidate(event.candidate);
-      console.log('Local ICE candidate: \n' + event.candidate.candidate);
+  var peerConn;
+  var dataChannel;
+
+  function signalingMessageCallback(message) {
+    if (message.type === 'offer') {
+      console.log('Got offer. Sending answer to peer.');
+      peerConn.setRemoteDescription(new RTCSessionDescription(message), function(){}, logError);
+      peerConn.createAnswer(onLocalSessionCreated, logError);
+
+    } else if (message.type === 'answer') {
+      console.log('Got answer.');
+      peerConn.setRemoteDescription(new RTCSessionDescription(message), function(){}, logError);
+
+    } else if (message.type === 'candidate') {
+      peerConn.addIceCandidate(new RTCIceCandidate({candidate: message.candidate}));
+
+    } else if (message === 'bye') {
+      // TODO: cleanup RTC connection?
     }
   }
 
-  function handleSendChannelStateChange() {
-    var readyState = sendChannel.readyState;
-    console.log('Send channel state is: ' + readyState);
-    if (readyState == "open") {
-      $scope.message = '';
-      // dataChannelSend.disabled = false;
-      // dataChannelSend.focus();
-      // dataChannelSend.placeholder = "";
-      // sendButton.disabled = false;
-      // closeButton.disabled = false;
+  function createPeerConnection(isInitiator, config) {
+    console.log('Creating Peer connection as initiator?', isInitiator, 'config:', config);
+    peerConn = new RTCPeerConnection(config);
+
+    // send any ice candidates to the other peer
+    peerConn.onicecandidate = function (event) {
+      console.log('onIceCandidate event:', event);
+      if (event.candidate) {
+        sendMessage({
+          type: 'candidate',
+          label: event.candidate.sdpMLineIndex,
+          id: event.candidate.sdpMid,
+          candidate: event.candidate.candidate
+        });
+      } else {
+        console.log('End of candidates.');
+      }
+    };
+
+    if (isInitiator) {
+      console.log('Creating Data Channel');
+      dataChannel = peerConn.createDataChannel("photos");
+      onDataChannelCreated(dataChannel);
+
+      console.log('Creating an offer');
+      peerConn.createOffer(onLocalSessionCreated, logError);
     } else {
-      // dataChannelSend.disabled = true;
-      // sendButton.disabled = true;
-      // closeButton.disabled = true;
+      peerConn.ondatachannel = function (event) {
+        console.log('ondatachannel:', event.channel);
+        dataChannel = event.channel;
+        onDataChannelCreated(dataChannel);
+      };
     }
   }
 
-  function gotRemoteIceCandidate(event) {
-    console.log('remote ice callback');
-    if (event.candidate) {
-      localPeerConnection.addIceCandidate(event.candidate);
-      console.log('Remote ICE candidate: \n ' + event.candidate.candidate);
+  function onLocalSessionCreated(desc) {
+    console.log('local session created:', desc);
+    peerConn.setLocalDescription(desc, function () {
+      console.log('sending local desc:', peerConn.localDescription);
+      sendMessage(peerConn.localDescription);
+    }, logError);
+  }
+
+  function onDataChannelCreated(channel) {
+    console.log('onDataChannelCreated:', channel);
+
+    channel.onopen = function () {
+      console.log('CHANNEL opened!!!');
+    };
+
+    channel.onmessage = (webrtcDetectedBrowser == 'firefox') ?
+    receiveDataFirefoxFactory() :
+    receiveDataChromeFactory();
+  }
+
+  function receiveDataChromeFactory() {
+    var buf, count;
+
+    return function onmessage(event) {
+      if (typeof event.data === 'string') {
+        buf = window.buf = new Uint8ClampedArray(parseInt(event.data));
+        count = 0;
+        console.log('Expecting a total of ' + buf.byteLength + ' bytes');
+        return;
+      }
+
+      var data = new Uint8ClampedArray(event.data);
+      buf.set(data, count);
+
+      count += data.byteLength;
+      console.log('count: ' + count);
+
+      if (count == buf.byteLength) {
+        // we're done: all data chunks have been received
+        console.log('Done. Rendering photo.');
+        renderPhoto(buf);
+      }
     }
   }
 
-  function gotReceiveChannel(event) {
-    console.log('Receive Channel Callback');
-    receiveChannel = event.channel;
-    receiveChannel.onmessage = handleMessage;
-    receiveChannel.onopen = handleReceiveChannelStateChange;
-    receiveChannel.onclose = handleReceiveChannelStateChange;
+  function receiveDataFirefoxFactory() {
+    var count, total, parts;
+
+    return function onmessage(event) {
+      if (typeof event.data === 'string') {
+        total = parseInt(event.data);
+        parts = [];
+        count = 0;
+        console.log('Expecting a total of ' + total + ' bytes');
+        return;
+      }
+
+      parts.push(event.data);
+      count += event.data.size;
+      console.log('Got ' + event.data.size + ' byte(s), ' + (total - count) + ' to go.');
+
+      if (count == total) {
+        console.log('Assembling payload')
+        var buf = new Uint8ClampedArray(total);
+        var compose = function(i, pos) {
+          var reader = new FileReader();
+          reader.onload = function() {
+            buf.set(new Uint8ClampedArray(this.result), pos);
+            if (i + 1 == parts.length) {
+              console.log('Done. Rendering photo.');
+              renderPhoto(buf);
+            } else {
+              compose(i + 1, pos + this.result.byteLength);
+            }
+          };
+          reader.readAsArrayBuffer(parts[i]);
+        }
+        compose(0, 0);
+      }
+    }
   }
 
-  function gotLocalDescription(desc) {
-    localPeerConnection.setLocalDescription(desc);
-    console.log('Offer from localPeerConnection \n' + desc.sdp);
-    remotePeerConnection.setRemoteDescription(desc);
-    remotePeerConnection.createAnswer(gotRemoteDescription);
-  }
-  function gotRemoteDescription(desc) {
-    remotePeerConnection.setLocalDescription(desc);
-    console.log('Answer from remotePeerConnection \n' + desc.sdp);
-    localPeerConnection.setRemoteDescription(desc);
-  }
 
-  function handleMessage(event) {
-    console.log(event);
-    console.log('Received message: ' + event.data);
-
-    $scope.messageQueue.push({text: event.data});
-    // document.getElementById("dataChannelReceive").value = event.data;
-  }
-
-  $scope.sendData = function() {
-    sendChannel.send($scope.message);
-    console.log('Sent data: ' + $scope.message);
-  }
 });
--- server/config/socketio.js
+++ server/config/socketio.js
@@ -1,10 +1,12 @@
 /**
- * Socket.io configuration
- */
+* Socket.io configuration
+*/
 
 'use strict';
 
-var config = require('./environment');
+var config = require('./environment'),
+os = require('os'),
+clientsCounter = 0;
 
 // When the user disconnects.. perform this
 function onDisconnect(socket) {
@@ -39,13 +41,14 @@ module.exports = function (socketio) {
 
   socketio.on('connection', function (socket) {
     socket.address = socket.handshake.address !== null ?
-            socket.handshake.address.address + ':' + socket.handshake.address.port :
-            process.env.DOMAIN;
+    socket.handshake.address.address + ':' + socket.handshake.address.port :
+    process.env.DOMAIN;
 
     socket.connectedAt = new Date();
 
     // Call onDisconnect.
     socket.on('disconnect', function () {
+      clientsCounter--;
       onDisconnect(socket);
       console.info('[%s] DISCONNECTED', socket.address);
     });
@@ -53,5 +56,51 @@ module.exports = function (socketio) {
     // Call onConnect.
     onConnect(socket);
     console.info('[%s] CONNECTED', socket.address);
+
+
+    function log(){
+      var array = [">>> Message from server:"];
+      array.push.apply(array, arguments);
+      socket.emit('log', array);
+    }
+
+    socket.on('message', function (message) {
+      log('Client said:', message);
+      console.log(message);
+      // for a real app, would be room only (not broadcast)
+      socket.broadcast.emit('message', message);
+    });
+
+    socket.on('create or join', function (room) {
+      clientsCounter++;
+      // log('Request to create or join room ' + room);
+
+      // var numClients = socketio.sockets.clients(room).length;
+      // log('Room ' + room + ' has ' + numClients + ' client(s)');
+
+      if (clientsCounter === 0){
+        socket.join(room);
+        socket.emit('created', room, socket.id);
+
+      } else if (clientsCounter === 1) {
+        socket.join(room);
+        socket.emit('joined', room, socket.id);
+        socketio.sockets.in(room).emit('ready');
+
+      } else { // max two clients
+        socket.emit('full', room);
+      }
+    });
+
+    socket.on('ipaddr', function () {
+      var ifaces = os.networkInterfaces();
+      for (var dev in ifaces) {
+        ifaces[dev].forEach(function (details) {
+          if (details.family=='IPv4' && details.address != '127.0.0.1') {
+            socket.emit('ipaddr', details.address);
+          }
+        });
+      }
+    });
   });
 };

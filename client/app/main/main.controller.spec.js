5e15a196f063d68bed83e94fb003399c5e70a792
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2015-05-08 06:02:27 -0700

    Remove old main ctrl tests.

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -5,17 +5,4 @@ describe('Controller: MainCtrl', function () {
   // load the service's module
   beforeEach(module('streamrootTestApp'));
   beforeEach(module('socketMock'));
-
-  beforeEach(inject(function ($http, $controller, $rootScope, scope) {
-
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of things to the scope', function () {
-    $http.flush();
-    expect(scope.users.length).toBe(4);
-  });
 });
